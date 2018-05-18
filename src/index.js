8a03e3fc719375debb6d80d6ce30f5ade93015bd
Nahidul Islam <nahidul.islam.cs@gmail.com>
2018-05-18 12:14:38 +0300

    add main funcionality of a tic-tac-toe game.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 class Square extends React.Component {
   render() {
     return (
-      <button class="square" onClick={() => this.props.onClick()}>
+      <button class="square" onClick={this.props.onClick}>
         {this.props.value}
       </button>
     );
@@ -16,24 +16,36 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
   renderSquare(i) {
-    return <Square value = {this.state.squares[i]} onClick = { () => { this.handleClick(i) }}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
   }
-  handleClick(i){
-      const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+  handleClick(i) {
+    const winner = calculateWinner(this.state.squares);
+    if(winner || this.state.squares[i])
+      return;
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
-    const status = "Next player: X";
-
+    const winner = calculateWinner(this.state.squares);
+    const status = this.state.xIsNext ? "Next player: X" : "Next player: O";
+    let message = winner ? winner + " has won the game." : status;
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{message}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -69,7 +81,25 @@ class Game extends React.Component {
     );
   }
 }
-
+function calculateWinner(squares) {
+  let winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < winningCombination.length; i++) {
+    const [a, b, c] = winningCombination[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
